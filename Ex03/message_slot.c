6e36d7cc49dac446f5279ebb07c991e3dce9852b
sagipolaczek <sagi.polaczek@gmail.com>
2021-12-06 20:26:01 +0200

    Debugging...

--- Ex03/message_slot.c
+++ Ex03/message_slot.c
@@ -85,7 +85,8 @@ static ssize_t device_read(struct file* file, char __user* buffer, size_t length
     for (i = 0; i < msg_size; i++) {
         // status = put_user(msg_buffer[i], &buffer[i]); // maybe need fix
         status = get_user(buffer[i], &msg_buffer[i]);
-        printk("%s - device_read: written char: %c\n", DEVICE_FILE_NAME, msg_buffer[i]);
+        printk("%s - device_read: char to be written = %c, written char = %c\n", 
+                                    DEVICE_FILE_NAME, buffer[i], msg_buffer[i]);
         if (status != SUCCESS) {
             // TODO: raise error
         }
@@ -133,7 +134,8 @@ static ssize_t device_write(struct file* file, const char __user* buffer, size_t
     msg_buffer = node -> msg_buffer;
     for (i = 0; i < length; i++) {
         status = put_user(buffer[i], &msg_buffer[i]); // maybe need fix
-        printk("%s - device_write: written char: %c\n", DEVICE_FILE_NAME, msg_buffer[i]);
+        printk("%s - device_write: char to be written = %c, written char = %c\n", 
+                                                DEVICE_FILE_NAME, msg_buffer[i], buffer[i]);
         if (status != SUCCESS) {
             // TODO: raise error
         }
